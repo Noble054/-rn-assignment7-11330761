4ac1585bee3655d3a6e5eaa7ddfce5956960ad4d
Noble054 <atetteh325@st.ug.edu.gh>
2024-07-10 03:06:45 +0000

    Create CartContext.js

---
+++ assign7/contexts/CartContext.js
@@ -0,0 +1,37 @@
+import React, { createContext, useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    const loadCart = async () => {
+      const storedCart = await AsyncStorage.getItem('cart');
+      if (storedCart) {
+        setCart(JSON.parse(storedCart));
+      }
+    };
+
+    loadCart();
+  }, []);
+
+  const addToCart = async (item) => {
+    const newCart = [...cart, item];
+    setCart(newCart);
+    await AsyncStorage.setItem('cart', JSON.stringify(newCart));
+  };
+
+  const removeFromCart = async (item) => {
+    const newCart = cart.filter(cartItem => cartItem.id !== item.id);
+    setCart(newCart);
+    await AsyncStorage.setItem('cart', JSON.stringify(newCart));
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
